4ec28d004c041d89d16ef3c64bd9bf790d84fb5c
Maes Constantin <constantinmaes@gmail.com>
2023-03-14 08:39:31 +0100

    :sparkles: feat: create http server

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
+import express from 'express';
 import { createWriteStream } from 'fs';
 import moment from "moment";
 import pdf2html from 'pdf2html';
 
-
 const BASE_ELECTRICITY_URL = 'https://cdn.eneco.be/downloads/fr/general/tk/BC_032_01@YY@@MM@_FR_ENECO_POWER_FLEX.pdf'
+
+const app = express()
+const port = process.env.PORT || 3000;
 
 function downloadFile(year, month) {
     const dest = `./${year}-${month}.pdf`;
@@ -39,14 +42,24 @@ async function parseTrimestrialTariff(dest) {
     const arr = raw.split('\n').filter(l => l !== '').find(l => l.toLowerCase().includes('tarif trimestriel')).split(' ').map(el => el.replace(',', '.'));
     const dayTariff = parseFloat(arr[1]);
     const nightTariff = parseFloat(arr[2]);
-    return { dayTariff, nightTariff };
+    return { peak: dayTariff, offpeak: nightTariff };
 }
 
-(async () => {
+const fetchTariffs = async () => {
     const month = process.argv.find(a => a.includes('month'))?.split('=')[1].padStart(2, '0') || moment().format('MM');
     const year = process.argv.find(a => a.includes('year'))?.split('=')[1].slice(-2) || moment().format('YY');
     console.log(`Fetching prices for ${month}-${year}`);
     const dest = await downloadFile(year, month);
     const tariffs = await parseTrimestrialTariff(dest);
     console.log(tariffs.dayTariff, tariffs.nightTariff);
-})();
+    return tariffs;
+};
+
+app.get('/', async (req, res) => {
+    const data = await fetchTariffs();
+    return res.status(200).json(data);
+})
+
+app.listen(port, () => {
+    console.log(`Eneco parser app listening on port ${port}`)
+})
